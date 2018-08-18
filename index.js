a75f789a2aff0db92b9bb42b7e9e527a2788f5fb
tatiana.mojica@bustle.com <tatiana.mojica@bustle.com>
2018-08-18 16:53:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // write your code below!
 describe('happyHolidays(string)', function () {
-    it('returns "Happy holidays!"', function () {
-      expect(happyHolidays('HAPPY HOLIDAYS')).toEqual('Happy holidays!')
+  it('returns "Happy holidays!" if `string` is in all caps', function () {
+    expect(happyHolidays('HAPPY HOLIDAYS')).toEqual('Happy holidays!')
   })
 })
+describe('happyHolidaysTo(string)', function ()
+  it('returns ')
