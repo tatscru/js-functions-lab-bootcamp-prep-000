bebd838466bb9d122cc986541294f66dfea552e2
tatiana.mojica@bustle.com <tatiana.mojica@bustle.com>
2018-08-18 16:44:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 describe('happyHolidays(string)', function () {
   it('returns,"Happy holidays!"', function () {
-    expect(HappyHolidays('HAPPY HOLIDAY')).toEqual('Happy holidays!')
+    expect(happyHolidays()).toEqual('Happy holidays!')
   })
 })
