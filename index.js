0f9c483726e163802a6a6642fae18beb3f31e638
tatiana.mojica@bustle.com <tatiana.mojica@bustle.com>
2018-08-18 17:37:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ function happyHolidays () {
   function happyHolidaysTo(name) {
     return `Happy holidays, ${name}!`
   }
-  function happyHolidaysTo(holiday, name) {
+  function happyHolidayTo(holiday, name) {
     return `Happy ${holiday}, ${name}!`
   }
